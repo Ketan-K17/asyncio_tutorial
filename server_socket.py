9ec651c6c2eea4b2c197fb826f8da25b2d23d662
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2026-07-20 21:45:15 +0530

    Converted sockets to non-blocking mode, and ignore blockingIOerror exceptions to make a socket server that can work with multiple clients at a time

--- server_socket.py
+++ server_socket.py
@@ -10,26 +10,38 @@ server_socket.bind(server_address)
 
 # make the server socket listen for connections on above address, the accept() method then blocks until we have a connection, that method will then return the client's socket object, and its address
 server_socket.listen()
+
+# mark server_socker as non-blocking
+server_socket.setblocking(False)
+
 print("waiting for connections...")
 
 
 connections = []
 try:
     while True:
-        connection, client_address = server_socket.accept()
-        print(f'I got a connection from {client_address}!')
-        connections.append(connection)
+        try:
+            connection, client_address = server_socket.accept()
+            connection.setblocking(False)
+            print(f'I got a connection from {client_address}!')
+            connections.append(connection)
+        except BlockingIOError: # you get this error because the given socket may have no data, at which point it asks you to check later..
+            pass
         for connection in connections:
-            buffer = b''
-            while buffer[-2:] != b'\r\n':
-                data = connection.recv(2)
-                if not data:
-                    break
-                else:
-                    print(f'I got data: {data}!')
-                    buffer = buffer + data
+            try:
+                buffer = b''
+                while buffer[-2:] != b'\r\n':
+                    data = connection.recv(2)
+                    if not data:
+                        break
+                    else:
+                        print(f'I got data: {data}!')
+                        buffer = buffer + data
 
-            print(f"All the data is: {buffer}")
-            connection.sendall(buffer)
+                print(f"All the data is: {buffer}")
+                connection.sendall(buffer)
+            
+            except BlockingIOError:
+                pass 
 finally:
     server_socket.close()
