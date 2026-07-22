485c474abd54f7daa3388a055380df4a30d5d582
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2026-07-22 23:38:38 +0530

    implemented the echo server in asyncio completely, reasoned why some select coroutine functions deserve to not be wrapped into tasks

--- server_socket.py
+++ server_socket.py
@@ -1,53 +1,30 @@
-"""
-What we've built is akin to a big part of what asyncio's event loop does under the
-hood. In this case, the events that matter are sockets receiving data. Each iteration of
-our event loop and the asyncio event loop is triggered by either a socket event happening, or a timeout triggering an iteration of the loop. In the asyncio event loop,
-when any of these two things happen, coroutines that are waiting to run will do so until they either complete or they hit the next await statement. When we hit an await
-in a coroutine that utilizes a non-blocking socket, it will register that socket with the
-system's selector and keep track that the coroutine is paused waiting for a result. We
-can translate this into pseudocode that demonstrates the concept:
-
-paused = []
-ready = []
-while True:
-    paused, new_sockets = run_ready_tasks(ready)
-    selector.register(new_sockets)
-    timeout = calculate_timeout()
-    events = selector.select(timeout)
-    ready = process_events(events)
-"""
-
-import selectors
+import asyncio
 import socket
-from selectors import SelectorKey
-from typing import List, Tuple
+from asyncio import AbstractEventLoop
 
-selector = selectors.DefaultSelector()
+async def echo(connection: socket, loop: AbstractEventLoop) -> None:
+    while data := await loop.sock_recv(connection, 1024):
+        data = b'hi, ' + data
+        await loop.sock_sendall(connection, data)
 
-server_socket = socket.socket()
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_address = ('127.0.0.1', 8000)
-server_socket.setblocking(False)
-server_socket.bind(server_address)
-server_socket.listen()
+async def listen_for_connection(server_socket: socket, loop: AbstractEventLoop):
+    while True:
+        connection, address = await loop.sock_accept(server_socket)
+        connection.setblocking(False)
 
-selector.register(server_socket, selectors.EVENT_READ)
+        print(f"Got a connection from {address}")
+        asyncio.create_task(echo(connection, loop))
 
-while True:
-    events: List[Tuple[SelectorKey, int]] = selector.select(timeout=1)
-    if len(events) == 0:
-        print('No events, waiting a bit more!')
+async def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_address = ('127.0.0.1', 8000)
+    server_socket.setblocking(False)
+    server_socket.bind(server_address)
+    server_socket.listen()
+    await listen_for_connection(server_socket, asyncio.get_event_loop())
 
-    for event, _ in events:
-        event_socket = event.fileobj
-        if event_socket == server_socket:
-            connection, address = server_socket.accept()
-            connection.setblocking(False)
-            print(f"I got a connection from {address}")
-            selector.register(connection, selectors.EVENT_READ)
 
-        else:
-            data = event_socket.recv(1024)
-            print(f"I got some data: {data}")
-            event_socket.send(data)
+asyncio.run(main())
 
+# listen_for_connections is your one central coroutine (qualitatively the same as main) that sets off other coroutines, so it makes more sense to not have it wrapped into a task, and just plain awaited. code won't break if you wrapped that into a task too.. in fact, say you wanted to create 5 server sockets, then wrapping it into tasks would make perfect sense.
